e5957d32535a0feffb32f125363b7b21f018d71e
footoredo <footoredo@gmail.com>
2016-01-27 21:52:27 +0800

    Supporting dragging timeline

--- danmu.js
+++ danmu.js
@@ -1,35 +1,36 @@
 var danmu = new Array();
 var displayed = new Array();
-var api, danmu_input;
+var api, danmu_input, danmu_check, last_check;
 
 var shoot_danmu = function(obj) {
-  danmu_input.value = obj["content"]
-  setTimeout(function() { danmu_input.value = ""; }, 1000);
+	danmu_input.value = obj["content"]
+	setTimeout(function() { danmu_input.value = ""; }, 1000);
 }
 
 var search_danmu = function(time) {
 	var ret = new Array();
 	for (var x in danmu) {
 	  var obj = danmu[x];
-		if (obj["time"] < time && !displayed[obj["id"]]) {
+		if (obj["time"] >= last_check && obj["time"] < time) {
 		  displayed[obj["id"]] = true;
 			ret.push(obj);
 		}
 	}
+	last_check = time;
 	return ret;
 }
 
 var update_danmu = function() {
-  time = api.video.time
+	time = api.video.time
 	var to_display = search_danmu(time)
 	for (var x in to_display) {
-	  var obj = to_display[x];
+		var obj = to_display[x];
 		shoot_danmu(obj);
 	}
 }
 
 window.onload = function() {
-  api = flowplayer();
+	api = flowplayer();
 
 	danmu = [
 		{"id":1,"time":1.0,"content":"HAHA"},
@@ -40,6 +41,6 @@ window.onload = function() {
 	displayed = new Array();
 	danmu_input = document.getElementById('danmu')
 	
-	setInterval(update_danmu, 50);
+	danmu_check = setInterval(update_danmu, 50);
 };
 
