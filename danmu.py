6d384e6425930155f758d5e5f98d2765ae4b6525
footoredo <footoredo@gmail.com>
2016-02-05 13:40:10 +0000

    update

--- danmu.py
+++ danmu.py
@@ -3,38 +3,48 @@
 
 import web
 import json
+import database
+import settings
+import os
+import re
 
 urls = (
 	'/', 'index',
-	'/get', 'get',
+	'/get/video/(\w{40})', 'get_video',
 	'/send', 'send'
 )
 
 app = web.application(urls, globals())
 render = web.template.render('.')
-id_count = 5;
+db = database.DB()
+video_url = 'http://fvck.it/darling-talk'
+
+def get_url(video_url):
+    ret = re.findall(r'http\S+',os.popen("you-get -u %s" % repr(video_url)).read())[0]
+    return ret
 
 class index:
 	def GET(self):
-		return render.index()
+		return render.index(video_url)
 
-class get:
-  def GET(self):
-    danmu = [
-		  {"id":1,"time":1.0,"content":"HAHAHAHAHAHA"},
-		  {"id":2,"time":1.7,"content":"呵呵呵呵呵呵呵呵"},
-		  {"id":3,"time":8.6,"content":"傻逼"},
-		  {"id":4,"time":10.3,"content":"Right!"}]
-    return json.dumps(danmu)
+class get_video:
+    def std_time(self, danmu):
+        danmu["create_time"] = danmu["create_time"].isoformat()
+        danmu["update_time"] = danmu["update_time"].isoformat()
+        
+    def GET(self, sha):
+        web.header("Access-Control-Allow-Origin", settings.ALLOW_ORIGIN)
+        danmu = db.get_danmu("where video_id='%s'" % sha)
+        map(self.std_time, danmu)
+        return json.dumps(danmu)
 		
-class send(object):
-  def POST(self):
-    bullet = json.loads(web.data())
-    global id_count;
-    bullet["id"] = id_count;
-    id_count = id_count + 1;
-    return id_count;
+class send:
+    def POST(self):
+        web.header("Access-Control-Allow-Origin", settings.ALLOW_ORIGIN)
+        danmu = web.input(_method="post")
+        danmu["context"] = danmu["context"].encode('utf-8')
+        return db.send_danmu(**danmu);
 		
 if __name__ == "__main__":
-    #web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
-    app.run()
+        web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+        app.run()
--- static/danmu.js
+++ static/danmu.js
@@ -1,130 +1,178 @@
-var danmu = new Array();
+var danmu = new Array(), buffer = new Array();
 var displayed = new Array();
 var api, danmu_check, last_check, gun, id_count=5;
 var danmuplayerJQ, danmuplayer;
 var playerJQ, player;
+var on_the_run = [];
+var video_id;
 
-var get_top = function() {
-  var top = 0;
-  $(".bullet").each(function() {
-    var b = $(this);
-    if (b.position().left + b.width() >= danmuplayerJQ.width() && b.position().top + b.height() > top)
-      top = b.position().top + b.height();
-  });
+var sleep = function(milliseconds) {
+    var start = new Date().getTime();
+    for (var i=0; i<1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) break;
+    }
+}
 
-  return top + "px";
+var get_pos = function(h) {
+    on_the_run.forEach(function(a){if ($(a[1]).position().left+$(a[1]).width()<=danmuplayerJQ.width()) a[0]=-1})
+    on_the_run = on_the_run.sort(function(a,b){if (a[0]<b[0]||a[0]==b[0]&&$(a[1]).position().top>$(b[1]).position().top) return 1; else return -1});
+    while (on_the_run.length && on_the_run[on_the_run.length-1][0]==-1) on_the_run.pop();
+    var z=0, y=0;
+    if (on_the_run[0]) z = on_the_run[0][0];
+    for (var i=0; i<on_the_run.length && on_the_run[i][0]==z; i++)
+        if (y+h > $(on_the_run[i][1]).position().top) {
+            y = $(on_the_run[i][1]).position().top + $(on_the_run[i][1]).height();
+        }
+        else break;
+    if (y+h > playerJQ.height()) {
+        z++;
+        y=0;
+    }
+    console.log(z,y);
+    return [z,y];
 }
 
 var shoot_danmu = function(obj) {
-  console.log(obj["content"]);
-  var content = obj["content"];
-	var bullet = document.createElement("div");
-	bullet.setAttribute("class", "bullet");
-	bullet.id = obj["id"];
-	bullet.innerHTML = content;
-	bullet.style.left = danmuplayerJQ.width() + "px";
-	bullet.style.top = get_top();
-	danmuplayer.appendChild(bullet);
-	var JQ = $(".bullet#"+bullet.id);
-	JQ.animate({"left":-JQ.width()}, 10000, "linear", function() {
-	  JQ.remove();
-  });
+    //console.log(danmu.findIndex(function(x){return x==obj}));
+    var content = obj["context"];
+    var bullet = document.createElement("div");
+    bullet.setAttribute("class", "bullet");
+    bullet.id = obj["id"];
+    bullet.innerHTML = content;
+    bullet.style.left = danmuplayerJQ.width() + "px";
+    bullet.style.visibility = "hidden";
+    danmuplayer.appendChild(bullet);
+    var pos = get_pos($(bullet).height());
+    bullet.style.top = pos[1] + "px";
+    bullet.style.visibility = "visible";
+    var JQ = $(bullet);
+    on_the_run.push([pos[0],bullet]);
+    JQ.animate({"left":-JQ.width()}, 10000, "linear", function() {
+            JQ.remove();
+            });
+    if (api.paused) JQ.pause();
 }
 
-var search_danmu = function(time) {
-	var ret = new Array();
-	for (var x in danmu) {
-	  var obj = danmu[x];
-		if (obj["time"] >= last_check && obj["time"] < time) {
-		  displayed[obj["id"]] = true;
-			ret.push(obj);
-		}
-	}
-	last_check = time;
-	return ret;
+var buffer_timeout = function(i) {
+    return setTimeout(function(){
+       if (api.paused) return;
+       shoot_danmu(danmu[i]);
+       if (i+100 < danmu.length)
+            buffer.push(buffer_timeout(i+100));
+        buffer.shift();
+    }, 1000*(danmu[i]["send_time"]-api.video.time));
 }
 
-var update_danmu = function() {
-	time = api.video.time
-	var to_display = search_danmu(time)
-	for (var x in to_display) {
-		var obj = to_display[x];
-		shoot_danmu(obj);
-	}
+var buffer_danmu = function() {
+    var time = api.video.time, start;
+    for (var i=0; i<danmu.length; i++) {
+        if (danmu[i]["send_time"]-0.000001 > time) {
+            start = i; break;
+        }
+    }
+
+    console.log("buffer_time:"+time);
+    console.log("start:"+start);
+    for (var i=start; i<danmu.length && i-start<100; i++) {
+        if (buffer.find(function(x){x==danmu[i]})) continue;
+        var timeout = buffer_timeout(i);
+        buffer.push(timeout);
+    }
+}
+
+var clear_buffer = function() {
+    while (buffer.length) {
+        clearTimeout(buffer[0]);
+        buffer.shift();
+    }
 }
 
 var init_player = function() {
-  danmuplayer = document.createElement("div");
-  danmuplayer.setAttribute("class", "danmuplayer");
-  document.body.appendChild(danmuplayer);
-  danmuplayerJQ = $(".danmuplayer");
-  playerJQ = $(".flowplayer");
-  player = document.getElementsByClassName("flowplayer")[0];
+    danmuplayer = document.createElement("div");
+    danmuplayer.setAttribute("class", "danmuplayer");
+    danmuplayer.style.width = danmuplayer.style.height = "100%";
+    playerJQ = $(".fp-player");
+    player = document.getElementsByClassName("fp-player")[0];
+    player.appendChild(danmuplayer)
+    danmuplayerJQ = $(".danmuplayer");
 }
 
 var sync_player = function() {
-  danmuplayer.style.width = playerJQ.width() + "px";
-  danmuplayer.style.height = playerJQ.height() + "px";
-  danmuplayer.style.top = playerJQ.position().top + "px";
-  danmuplayer.style.left = playerJQ.position().left + "px";
+    danmuplayer.style.width = playerJQ.width() + "px";
+    danmuplayer.style.height = playerJQ.height() + "px";
+    danmuplayer.style.top = playerJQ.position().top + "px";
+    danmuplayer.style.left = playerJQ.position().left + "px";
 }
 
 window.onload = function() {
-	api = flowplayer();
+    api = flowplayer();
+    video_id = document.getElementsByClassName("flowplayer")[0].id;
+    init_player();
+    //sync_player();
+    //$(window).resize(function(){sync_player()});
 
-	$.get("/get", function(data, status) {
-	  danmu = JSON.parse(data);
-	})
-	displayed = new Array();
-	danmu_displayer = document.getElementById('danmu');
-	gun = document.getElementById('gun');
-	
-	danmu_check = setInterval(update_danmu, 50);
-	init_player();
-	sync_player();
-	$(window).resize(function(){sync_player()});
-	
-  api.on("pause", function() {
-    $(".bullet").pause();
-  });
+    $.get("http://danmu.footoredo.cat/get/video/" + video_id, function(data, status) {
+            danmu = JSON.parse(data);
+            danmu = danmu.sort(function(a,b){return a["send_time"]-b["send_time"]});
+            if (api.playing) buffer_danmu();
+            })
 
-  api.on("resume", function() {
-    $(".bullet").resume();
-  });
+    gun = document.getElementById('gun');
 
-  api.on("seek", function() {
-    last_check = api.video.time;
-    $(".bullet").remove();
-  });
-  
-  api.on("fullscreen", function() {
-    $(window).unbind("resize");
-    danmuplayer.style.width = "100%";
-    danmuplayer.style.height = "100%";
-    danmuplayer.style.top = "0";
-    danmuplayer.style.left = "0";
-  });
-  
-  api.on("fullscreen-exit", function() {
-    sync_player();
-    $(window).resize(function(){sync_player()});
-  });
+    api.on("pause", function() {
+                clear_buffer();
+                $(".bullet").pause();
+            });
+
+    api.on("resume", function() {
+            $(".bullet").resume();
+            buffer_danmu();
+            });
+
+    api.on("seek", function() {
+            if (api.playing) {
+                clear_buffer();
+                $(".bullet").remove();
+                buffer_danmu();
+            }
+            else {
+                $(".bullet").remove();
+            }
+            });
+
+    /*api.on("fullscreen", function() {
+            $(window).unbind("resize");
+            danmuplayer.style.width = "100%";
+            danmuplayer.style.height = "100%";
+            danmuplayer.style.top = "0";
+            danmuplayer.style.left = "0";
+            });
+
+    api.on("fullscreen-exit", function() {
+            sync_player();
+            $(window).resize(function(){sync_player()});
+            });*/
 };
 
 
 var fire_danmu = function() {
-	var bullet = {"time":api.video.time,"content":gun.value};
-	if (gun.value.length > 50) {
-	  alert("太长了傻逼");
-	  return false;
-  }
-	gun.value = "";
-	$.post("/send", JSON.stringify(bullet), function(data, status) {
-	  console.log(data);
-	  bullet["id"] = data;
-	  shoot_danmu(bullet);
-	  setTimeout(function(){danmu.push(bullet);}, 500);
-	});
-	return false;
+    var bullet = {"send_time":api.video.time,"context":gun.value};
+    bullet["video_id"] = video_id;
+    bullet["user_id"] = "";
+
+        if (gun.value.length > 50) {
+            alert("太长了傻逼");
+            return false;
+        }
+    gun.value = "";
+    console.log(bullet);
+    $.post("/send", bullet, function(data, status) {
+            console.log(data);
+            console.log(status);
+            bullet["id"] = data;
+            shoot_danmu(bullet);
+            setTimeout(function(){danmu.push(bullet);}, 500);
+            });
+    return false;
 }
 
